62353cad6d7b74b57beecca5ea78c9b155c65013
sujeet kumar <sujeetkr503@gmail.com>
2025-01-24 07:52:46 +0530

    npm init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const app=express();
+const port =8080;
+const path=require("path");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+    res.send("Qoura")
+    // console.log("post");
+})
+
+
+app.listen(port,(req,res)=>{
+    console.log("Server is running on port "+port);
+});
